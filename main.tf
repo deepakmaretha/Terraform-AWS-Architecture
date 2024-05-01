1edcf7e4d718b69bdd4b5f3ae2ccf6f52f0490b5
deepakmaretha <deepakmaretha6@gmail.com>
2024-05-01 23:46:00 +0530

    Formatting fixed

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_instance" "webserver1" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tfsg.id]
-  subnet_id = aws_subnet.tfsubnet1.id
-  user_data = base64encode(file(var.userdata))
-  key_name = aws_key_pair.tfkey.key_name
+  subnet_id              = aws_subnet.tfsubnet1.id
+  user_data              = base64encode(file(var.userdata))
+  key_name               = aws_key_pair.tfkey.key_name
   tags = {
     Name = "TF-WEB-SERVER-1"
   }
 }
 
 resource "aws_instance" "webserver2" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tfsg.id]
-  subnet_id = aws_subnet.tfsubnet2.id
-  user_data = base64encode(file(var.userdata2))
-  key_name = aws_key_pair.tfkey.key_name
+  subnet_id              = aws_subnet.tfsubnet2.id
+  user_data              = base64encode(file(var.userdata2))
+  key_name               = aws_key_pair.tfkey.key_name
   tags = {
     Name = "TF-WEB-SERVER-2"
   }
--- networking.tf
+++ networking.tf
@@ -1,28 +1,28 @@
 # CREATING VPC AND PRIVATE SUBNETS 
 
 resource "aws_vpc" "tfvpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
+  cidr_block = "10.0.0.0/16"
+  tags = {
     Name = "tfvpc"
   }
 }
 
 resource "aws_subnet" "tfsubnet1" {
-    vpc_id = aws_vpc.tfvpc.id
-    cidr_block = "10.0.0.0/24"
-    availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = true
-    tags = {
+  vpc_id                  = aws_vpc.tfvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
     Name = "tf-subnet-1"
   }
 }
 
 resource "aws_subnet" "tfsubnet2" {
-    vpc_id = aws_vpc.tfvpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
-    tags = {
+  vpc_id                  = aws_vpc.tfvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
     Name = "tf-subnet-2"
   }
 }
@@ -30,30 +30,30 @@ resource "aws_subnet" "tfsubnet2" {
 # CREATING INTERNET GATEWAY, ROUTE TABLE AND ASSOCIATION
 
 resource "aws_internet_gateway" "tfigw" {
-    vpc_id = aws_vpc.tfvpc.id
-    tags = {
+  vpc_id = aws_vpc.tfvpc.id
+  tags = {
     Name = "tf-internet-gateway"
   }
 }
 
 resource "aws_route_table" "tfrt" {
-    vpc_id = aws_vpc.tfvpc.id
-    
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.tfigw.id
-    }
-    tags = {
+  vpc_id = aws_vpc.tfvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tfigw.id
+  }
+  tags = {
     Name = "tf-route-table"
   }
 }
 resource "aws_route_table_association" "rta1" {
-  subnet_id = aws_subnet.tfsubnet1.id
+  subnet_id      = aws_subnet.tfsubnet1.id
   route_table_id = aws_route_table.tfrt.id
 }
 
 resource "aws_route_table_association" "rta2" {
-  subnet_id = aws_subnet.tfsubnet2.id
+  subnet_id      = aws_subnet.tfsubnet2.id
   route_table_id = aws_route_table.tfrt.id
 }
 
@@ -61,20 +61,20 @@ resource "aws_route_table_association" "rta2" {
 # CREATE APPLICATION LOAD BALANCER and TARGET GROUP
 
 resource "aws_lb" "tflb" {
-  name = "tf-lb"
-  internal = false
+  name               = "tf-lb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.tfsg.id]
-  subnets = [aws_subnet.tfsubnet1.id, aws_subnet.tfsubnet2.id]
+  security_groups    = [aws_security_group.tfsg.id]
+  subnets            = [aws_subnet.tfsubnet1.id, aws_subnet.tfsubnet2.id]
   tags = {
     Name = "Terraform ALB"
   }
 }
 resource "aws_lb_target_group" "tftg" {
-  name = "tftg"
-  port = 80
+  name     = "tftg"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = aws_vpc.tfvpc.id
+  vpc_id   = aws_vpc.tfvpc.id
 
   health_check {
     path = "/"
@@ -86,22 +86,22 @@ resource "aws_lb_target_group" "tftg" {
 
 resource "aws_lb_target_group_attachment" "attach1" {
   target_group_arn = aws_lb_target_group.tftg.arn
-  target_id = aws_instance.webserver1.id
-  port = 80
+  target_id        = aws_instance.webserver1.id
+  port             = 80
 }
 resource "aws_lb_target_group_attachment" "attach2" {
   target_group_arn = aws_lb_target_group.tftg.arn
-  target_id = aws_instance.webserver2.id
-  port = 80
+  target_id        = aws_instance.webserver2.id
+  port             = 80
 }
 
 resource "aws_lb_listener" "tflistener" {
   load_balancer_arn = aws_lb.tflb.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-   target_group_arn = aws_lb_target_group.tftg.arn
-   type = "forward"
+    target_group_arn = aws_lb_target_group.tftg.arn
+    type             = "forward"
   }
 }
